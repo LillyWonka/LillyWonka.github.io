8925476d7f12132e230da13526db4784160f4612
Liliia Harkushko <wonka.superme@gmail.com>
2025-03-20 00:39:04 +0200

    added GA, GA disclaimer, commented index content
    New design and content coming soon. Rethinking the concept of the site and creating design from scratch

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
-let subscribeBtn = document.querySelector('.subscribe');
-subscribeBtn.addEventListener('click',openSubscribe);
-function openSubscribe(){
-    document.querySelector(".subscribeField").style.display = "block";
-    document.querySelector(".overlay").style.display = "block";
-}
+// let subscribeBtn = document.querySelector('.subscribe');
+// subscribeBtn.addEventListener('click',openSubscribe);
+// function openSubscribe(){
+//     document.querySelector(".subscribeField").style.display = "block";
+//     document.querySelector(".overlay").style.display = "block";
+// }
 
-let closeBtn = document.querySelector('.closeSubscribe');
-closeBtn.addEventListener('click',closeSubscribe);
-function closeSubscribe(){
-    document.querySelector(".subscribeField").style.display = "none";
-    document.querySelector(".overlay").style.display = "none";
-}
+// let closeBtn = document.querySelector('.closeSubscribe');
+// closeBtn.addEventListener('click',closeSubscribe);
+// function closeSubscribe(){
+//     document.querySelector(".subscribeField").style.display = "none";
+//     document.querySelector(".overlay").style.display = "none";
+// }
 
-let subscribeButton=document.querySelector('.subscribe-everywhere');
-if (subscribeButton){
-subscribeButton.addEventListener('click', openSubscribe);
-}
-let scroll=document.querySelector('a.scroll');
-if (scroll){
-scroll.addEventListener('click', scrollDown)
-function scrollDown() {
-    scroll.scrollIntoView({
-        block: 'start',
-        behavior: 'smooth'
-})
-}
+// let subscribeButton=document.querySelector('.subscribe-everywhere');
+// if (subscribeButton){
+// subscribeButton.addEventListener('click', openSubscribe);
+// }
+// let scroll=document.querySelector('a.scroll');
+// if (scroll){
+// scroll.addEventListener('click', scrollDown)
+// function scrollDown() {
+//     scroll.scrollIntoView({
+//         block: 'start',
+//         behavior: 'smooth'
+// })
+// }
+// }
+
+let closeAck = document.querySelector('#disclaimer_ack');
+closeAck.addEventListener('click',closeAcknowledgement);
+function closeAcknowledgement(){
+    document.querySelector(".disclaimer").style.display = "none";
 }
